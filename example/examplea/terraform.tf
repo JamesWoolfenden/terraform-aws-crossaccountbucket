8dae5991797b0855f9946eed8d30998b01eec162
James Woolfenden <james.woolfenden@gmail.com>
2022-09-20 17:09:14 +0100

    pike

--- example/examplea/terraform.tf
+++ example/examplea/terraform.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      version               = "3.39.0"
-      source                = "hashicorp/aws"
-      configuration_aliases = [aws.prod]
+      version = "4.31.0"
+      source  = "hashicorp/aws"
     }
 
     null = {
